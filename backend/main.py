845f25d21b03c01827f010a340fb6ec7e60120c9
loxigl <43906792+loxigl@users.noreply.github.com>
2023-10-07 13:33:51 +0300

    telegram bot endpoints

--- backend/main.py
+++ backend/main.py
@@ -13,15 +13,14 @@ class DictObj:
         assert isinstance(in_dict, dict)
         for key, val in in_dict.items():
             if isinstance(val, (list, tuple)):
-               setattr(self, key, [DictObj(x) if isinstance(x, dict) else x for x in val])
+                setattr(self, key, [DictObj(x) if isinstance(x, dict) else x for x in val])
             else:
-               setattr(self, key, DictObj(val) if isinstance(val, dict) else val)
+                setattr(self, key, DictObj(val) if isinstance(val, dict) else val)
 
 
 app = FastAPI()
 
-dotenv.load_dotenv('.env')
-
+dotenv.load_dotenv(os.getcwd() + "/backend/.env")
 token = os.environ.get("INFLUXDB_TOKEN")
 org = "my-org"
 url = "http://62.109.26.57:8086"
@@ -42,10 +41,33 @@ async def root(timestamp: str):
     for t in tables:
         record = t.records
         record[0].row = None
-        my_obj = DictObj(record[0].values)
-        records.append(my_obj)
+        records += record
 
     return records
+
+
+@app.get("/api/get/average")
+async def root(timestamp: str):
+    query_api = write_client.query_api()
+
+    query = """from(bucket: "sensors")
+     |> range(start: -""" + timestamp + """)
+     """
+    tables = query_api.query(query, org="my-org")
+    records = []
+    for t in tables:
+        record = t.records
+        record[0].row = None
+        records += record
+    sum_hum = 0
+    sum_tem = 0
+    count = 0
+    for r in records:
+        sum_hum += float(r.values["humidity"])
+        sum_tem += float(r.values["temperature"])
+        count += 1
+    result = {"avg_temp": sum_tem / count, "avg_humidity": sum_hum / count}
+    return result
 
 
 @app.post("/api/set")
