7de0dffa9dd5bfd6cc8bb4f2b93b5fe6f0037f92
loxigl <43906792+loxigl@users.noreply.github.com>
2023-10-07 19:40:05 +0300

    no websocket anymore

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 import json
 from dataclasses import dataclass
 
+import uvicorn
 from fastapi import FastAPI, WebSocket
 import influxdb_client, os, time
 from influxdb_client import InfluxDBClient, Point, WritePrecision
@@ -19,40 +20,13 @@ class DictObj:
 
 
 app = FastAPI()
-
-dotenv.load_dotenv(os.getcwd() + "/backend/.env")
+print(os.getcwd())
+dotenv.load_dotenv(os.getcwd() + "/.env")
 token = os.environ.get("INFLUXDB_TOKEN")
 org = "my-org"
 url = "http://62.109.26.57:8086"
 
 write_client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
-
-
-@app.websocket("/api/ws")
-async def websocket_endpoint(websocket: WebSocket, timestamp: str):
-    await websocket.accept()
-    err = False
-
-    while (True):
-        records = []
-        timestamp = timestamp
-        query_api = write_client.query_api()
-        query = """from(bucket: "sensors")
-                             |> range(start: """ + timestamp + """)
-
-                             """
-        tables = query_api.query(query, org="my-org")
-        result = []
-        for t in tables:
-            record = t.records
-            record[0].row = None
-            records += record
-        for r in records:
-            result += [{'time': r.values["_time"].time().strftime("%H:%M:%S"), "humidity": r.values['humidity'],
-                        "temperature": r.values["temperature"]}]
-        data = str(result)
-        time.sleep(5)
-        await websocket.send_text(data)
 
 
 @app.get("/api/get/humidity")
@@ -79,7 +53,6 @@ async def get_temperature(timestamp: str):
     query_api = write_client.query_api()
     query = """from(bucket: "sensors")
          |> range(start: """ + timestamp + """)
-
          """
     tables = query_api.query(query, org="my-org")
     records = []
@@ -142,3 +115,7 @@ async def setter(sensor_id: int, temperature: float, humidity: float):
     )
 
     return write_api.write(bucket=bucket, org="my-org", record=point)
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, port=8080, host='192.168.138.64')
